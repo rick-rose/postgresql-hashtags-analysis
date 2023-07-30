160e9663216b7edc3b15adbecbb2e657ac1a1bdf
Rick Rose <138879725+rick-rose@users.noreply.github.com>
2023-07-30 15:02:24 -0400

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -7,14 +7,13 @@ create table if not exists post
 -- --------------------
 -- DENORMALIZED Method
 -- --------------------
-create table if not exists post_tag
+create table if not exists post_tag_denormalized
 (
 	id bigserial not null constraint table_name_pk primary key,
 	post_id bigint not null,
 	tag text not null
 );
-
-create index if not exists post_tag_tag_index on post_tag (tag);
+create index if not exists post_tag_denormalized_tag_index on post_tag (tag);
 
 -- --------------------
 -- NORMALIZED Method
@@ -24,37 +23,36 @@ create table if not exists tag
 	id bigserial not null constraint tag_pk primary key,
 	name text not null
 );
+create unique index if not exists tag_uindex on tag (name);
 
-create table if not exists post_tag
+create table if not exists post_tag_normalized
 (
 	id bigserial not null constraint post_tag_pk primary key,
-	post_id bigint not null constraint post_tag_post_id_fk references post,
-	tag_id bigint not null constraint post_tag_tag_id_fk references tag
+	post_id bigint not null constraint post_tag_normalized_post_id_fk references post,
+	tag_id bigint not null constraint post_tag_normalized_tag_id_fk references tag
 );
-
-create unique index if not exists post_tag_post_id_tag_id_uindex on post_tag (post_id, tag_id);
-create unique index if not exists tag_uindex on tag (name);
+create unique index if not exists post_tag_normalized_post_id_tag_id_uindex on post_tag_normalized (post_id, tag_id);
 
 -- --------------------
 -- JSONB Method
 -- --------------------
-create table if not exists post_tag
+create table if not exists post_tag_josnb
 (
-	post_id bigint not null constraint post_tag_pk primary key constraint post_tag_json_post_id_fk references post,
+	post_id bigint not null constraint post_tag_josnb_pk primary key constraint post_tag_josnb_post_id_fk references post,
 	tags jsonb
 );
 
-create extensioin pg_trgm;
-create index concurrently post_tag_tags_gin on post_tag using gin (tags);
+create extension pg_trgm;
+create index concurrently post_tag_josnb_tags_gin on post_tag_josnb using gin (tags);
 
 -- --------------------
 -- ARRAY Method
 -- --------------------
-create table if not exists post_tag
+create table if not exists post_tag_array
 (
-	post_id bigint not null constraint post_tag_pk primary key constraint post_tag_post_id_fk references post,
+	post_id bigint not null constraint post_tag_array_pk primary key constraint post_tag_array_post_id_fk references post,
 	tags text[] default '{}'::text[] not null
 );
 
-create extensioin pg_trgm;
-create index concurrently post_tag_tags_gin on post_tag using gin (tags);
+create extension pg_trgm;
+create index concurrently post_tag_array_tags_gin on post_tag using gin (tags);
